7088c73de91414749d2c7c6d8fc0f1c3481f2296
Yashodhan Singh <35937088+YashodhanSingh@users.noreply.github.com>
2019-11-05 17:54:13 -0500

    Miniarticle.js with gridded layout

--- frontend/src/components/Mini-article.js
+++ frontend/src/components/Mini-article.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
     root: {
+        flexGrow:1,
         textAlign: "left"
+    },
+    control: {
+      padding:theme.spacing(2)
     },
     page: {
         display: "inline-block",
@@ -16,8 +22,7 @@ const useStyles = makeStyles({
     },
     title:{
         textTransform: "uppercase",
-        fontWeight: "200",
-        objectFit: "contain",
+        fontWeight: "400",
         textAlign: "left"
     },
     teaser: {
@@ -27,29 +32,39 @@ const useStyles = makeStyles({
     author: {
         textAlign: "left"
     },
-    containerBox:{
-        textAlign: "left"
-    }
-});
+}));
 
 const MiniArticleComponent = (props) => {
     const classes = useStyles()
     props = props.props
     return (
-        <div className = {classes.root}>
-            <div className = {classes.page}>
-                <img className= 'bannerImage' alt ="banner for mini article component" src = {props.banner} style = {{ maxWidth: "300px"}}/>
-                <Typography variant="h6" className = {classes.title}>
-                    {props.title}
-                </Typography>
-                <Typography variant="h7" className = {classes.author} style = {{ fontWeight: "200"}}>
-                    {props.author}
-                </Typography>
-                <Typography variant="subtitle1" className = {classes.teaser}>
-                    {props.teaser}
-                </Typography>
-            </div>
-        </div>
+      <div className={classes.page}>
+        <Grid container className = {classes.root} spacing = {2}>
+          <Grid item xs={12}>
+            <Grid container justify = "center" spacing = {10}>
+              {[0, 1, 2, 3, 4, 5, 6, 7, 8].map(value => (
+                <Grid key ={value} item>
+                <Box
+                  width={300}
+                  lineHeight={1}
+                  float="left">
+                  <img className= 'bannerImage' alt ="banner for mini article component" src = {props.banner} style = {{ maxWidth: "300px"}}/>
+                  <Typography variant="h6" className = {classes.title}>
+                      {props.title}
+                  </Typography>
+                  <Typography variant="h7" className = {classes.author} style = {{ fontWeight: "200"}}>
+                      {props.author}
+                  </Typography>
+                  <Typography variant="subtitle1" className = {classes.teaser}>
+                      {props.teaser}
+                  </Typography>
+                </Box>
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
     )
 }
 
@@ -69,7 +84,7 @@ class MiniArticle extends Component {
 MiniArticle.defaultProps = {
     banner: "ASSETS/trumpCartoon.jpg",
     title: "Dummy Title goes here",
-    teaser: "A teaser for the dummy article here",
+    teaser: "A teaser for the dummy article here and even more teaser",
     author: "Dummy Author 1"
 }
 
