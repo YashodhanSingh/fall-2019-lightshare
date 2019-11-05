80d83342365152b5b55da4d5fcaa6ebedb69153e
Yashodhan Singh <35937088+YashodhanSingh@users.noreply.github.com>
2019-11-05 16:52:13 -0500

    Updating mini-article.js
    works and looks okay for 1 unit but need to make it so multiple can go next to each other

--- frontend/src/components/Mini-article.js
+++ frontend/src/components/Mini-article.js
@@ -1,28 +1,77 @@
 import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles({
+    root: {
+        textAlign: "left"
+    },
+    page: {
+        display: "inline-block",
+        width: "100%",
+        maxWidth:"1080px"
+    },
+    bannerImage: {
 
-class Miniarticle extends Component {
-  
-  render() {
+    },
+    title:{
+        textTransform: "uppercase",
+        fontWeight: "200",
+        objectFit: "contain",
+        textAlign: "left"
+    },
+    teaser: {
+        textAlign: "left",
+        fontStyle: "italic"
+    },
+    author: {
+        textAlign: "left"
+    },
+    containerBox:{
+        textAlign: "left"
+    }
+});
+
+const MiniArticleComponent = (props) => {
+    const classes = useStyles()
+    props = props.props
     return (
-      <div>
-          <img alt = "Banner" src={this.props.imgURL} width = "300" height = "200"/>
-          <h1>{this.props.title}</h1>
-          <h4>By {this.props.author}</h4>
-          <h4>{this.props.datePublised}</h4>
-          <p>{this.props.content}</p>
-      </div>
-    );
-  }
+        <div className = {classes.root}>
+            <div className = {classes.page}>
+                <img className= 'bannerImage' alt ="banner for mini article component" src = {props.banner} style = {{ maxWidth: "300px"}}/>
+                <Typography variant="h6" className = {classes.title}>
+                    {props.title}
+                </Typography>
+                <Typography variant="h7" className = {classes.author} style = {{ fontWeight: "200"}}>
+                    {props.author}
+                </Typography>
+                <Typography variant="subtitle1" className = {classes.teaser}>
+                    {props.teaser}
+                </Typography>
+            </div>
+        </div>
+    )
 }
 
-Miniarticle.defaultProps = {
-    title: "Default Title",
-    author: "Someone",
-    datePublised: "12/12/2020",
-    content: "Default Mini Article content",
-    imgURL: "ASSETS/defaultBanner.png"
+class MiniArticle extends Component {
+    constructor(props) {
+        super(props)
+        console.log(props)
+    }
 
+    render() {
+        return (
+            <MiniArticleComponent props = {this.props} />
+        )
+    }
 }
 
-export default Miniarticle;
+MiniArticle.defaultProps = {
+    banner: "ASSETS/trumpCartoon.jpg",
+    title: "Dummy Title goes here",
+    teaser: "A teaser for the dummy article here",
+    author: "Dummy Author 1"
+}
+
+export default MiniArticle
+
